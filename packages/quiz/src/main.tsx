df7e40a92bb3d986b8d8200fe779caaab07aa81c
Emil Hörnlund <emil.hornlund@me.com>
2024-09-29 10:38:11 +0200

    feat: Add Lato font assets and update styles
    - Added Lato font files (Black, BlackItalic, Bold, BoldItalic, Light, LightItalic, Regular, RegularItalic, Thin, ThinItalic).
    - Updated font-face declarations in `fonts.scss` for Lato in various weights and styles.
    - Modified `index.css` to use Lato as the primary font, along with a default font size of 16px.
    - Added color variables in `colors.scss` for various color shades (blue, yellow, pink, red, orange, green, turquoise, gray, white).
    - Added media breakpoint variables in `variables.scss` for responsive design.

--- packages/quiz/src/main.tsx
+++ packages/quiz/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { App } from './pages'
+import './styles/fonts.scss'
 import './styles/index.css'
 
 const router = createBrowserRouter([
