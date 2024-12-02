284d87c6069da876932adf4c8cedc9258851bac9
Emil Hörnlund <emil.hornlund@me.com>
2024-12-02 20:17:48 +0100

    feat(styles): Update disabled state styles and enhance Textarea component
    - Updated the disabled state styles for Select, TextField, and Textarea components:
      - Changed background color from `$gray-3` to `$gray-1`.
      - Adjusted outline color from `$gray-3` to `$gray-1`.
      - Updated text color from `$gray-4` to `$gray-2`.
    - Added missing `:disabled` styles to the Textarea SCSS module.
    - Enhanced the Textarea component:
      - Introduced `placeholder` and `disabled` props.
      - Set a default value for the `disabled` prop (`false`).
      - Updated `textarea` element to handle new props.
    
    Improves consistency across components and expands the flexibility of the Textarea component.

--- packages/quiz/src/components/Textarea/Textarea.tsx
+++ packages/quiz/src/components/Textarea/Textarea.tsx
@@ -6,16 +6,27 @@ export interface TextareaProps {
   id: string
   name?: string
   value?: string
+  placeholder?: string
+  disabled?: boolean
   onChange?: (value: string) => void
 }
 
-const Textarea: FC<TextareaProps> = ({ id, name = id, value, onChange }) => {
+const Textarea: FC<TextareaProps> = ({
+  id,
+  name = id,
+  value,
+  placeholder,
+  disabled = false,
+  onChange,
+}) => {
   return (
     <div className={styles.container}>
       <textarea
         id={id}
         name={name}
         value={value}
+        placeholder={placeholder}
+        disabled={disabled}
         onChange={(event) => onChange?.(event.target.value)}
         data-testid={`test-${id}-textarea`}
       />
