c378b349af501a0cfa85bbed558d41b3c017c92d
Emil Hörnlund <emil.hornlund@me.com>
2024-12-05 21:54:06 +0100

    feat(join-page): Pre-fill nickname using auth context and update player state
    - Added `useAuthContext` to `JoinPage` to access the current player's information and update the nickname field automatically if a player is already logged in.
    - Implemented a `useEffect` hook to pre-fill the nickname input field with the player's existing nickname from the auth context.
    - Updated the `handleSubmit` function to persist the updated nickname in the auth context before initiating the game join request.
    - Improved user experience by maintaining nickname consistency across sessions.
    
    These changes enhance the Join Page by integrating player information from the auth context, reducing redundant inputs for returning players, and improving overall usability.

--- packages/quiz/src/pages/JoinPage/JoinPage.tsx
+++ packages/quiz/src/pages/JoinPage/JoinPage.tsx
@@ -1,5 +1,5 @@
 import { PLAYER_NICKNAME_REGEX } from '@quiz/common'
-import React, { FC, FormEvent, useMemo, useState } from 'react'
+import React, { FC, FormEvent, useEffect, useMemo, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
 import { useQuizServiceClient } from '../../api/use-quiz-service-client.tsx'
@@ -10,6 +10,7 @@ import {
   TextField,
   Typography,
 } from '../../components'
+import { useAuthContext } from '../../context/auth'
 
 import { getMessage, getTitle } from './helpers.ts'
 import styles from './JoinPage.module.scss'
@@ -26,11 +27,22 @@ const JoinPage: FC = () => {
   const [nickname, setNickname] = useState<string>()
   const [nicknameValid, setNicknameValid] = useState<boolean>(false)
 
+  const { player, setPlayer } = useAuthContext()
+  useEffect(() => {
+    if (player?.nickname) {
+      setNickname(player.nickname)
+    }
+  }, [player])
+
   const title = useMemo<string>(() => getTitle(), [])
   const message = useMemo<string>(() => getMessage(), [])
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault()
+
+    if (player && nickname) {
+      setPlayer({ ...player, nickname })
+    }
 
     if (gameID && nickname) {
       joinGame(gameID, nickname)
