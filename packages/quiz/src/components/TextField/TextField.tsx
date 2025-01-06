a83d0c53129cea349fc2e4fb12a31562ee9b2117
Emil Hörnlund <emil.hornlund@me.com>
2025-01-06 11:40:05 +0100

    feat(quiz): Improve TextField component validation and error handling
    - Refactored `TextField` component to include enhanced validation with `minLength`, `maxLength`, and regex-based error messages.
    - Introduced a new `errorContainer` to display validation error messages alongside input fields.
    - Updated styles to replace `.textFieldInputContainer` with `.inputContainer` for improved structure.
    - Added new SCSS styles for `.input-error` to ensure consistent error message display across devices.
    - Modified snapshot tests to reflect the updated DOM structure and validation logic.
    - Updated snapshots in multiple components, including `QuizEditor` and `PlayerQuestionState`.
    
    Enhances user experience by providing clearer validation feedback and maintaining consistent styling across components.

--- packages/quiz/src/components/TextField/TextField.tsx
+++ packages/quiz/src/components/TextField/TextField.tsx
@@ -1,8 +1,11 @@
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCheck,
+  faTriangleExclamation,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { ChangeEvent, useEffect, useState } from 'react'
 
-import { classNames, isValidNumber } from '../../utils/helpers.ts'
+import { classNames } from '../../utils/helpers.ts'
 
 import styles from './TextField.module.scss'
 
@@ -15,9 +18,11 @@ export interface TextFieldProps {
   value?: string | number
   min?: number
   max?: number
-  regex?: RegExp
+  minLength?: number
+  maxLength?: number
+  regex?: RegExp | { value: RegExp; message: string }
   checked?: boolean
-  required?: boolean
+  required?: boolean | string
   disabled?: boolean
   readOnly?: boolean
   onChange?: (value?: string | number) => void
@@ -34,6 +39,8 @@ const TextField: React.FC<TextFieldProps> = ({
   value,
   min,
   max,
+  minLength,
+  maxLength,
   regex,
   checked,
   required,
@@ -52,29 +59,71 @@ const TextField: React.FC<TextFieldProps> = ({
   }, [value])
 
   const [valid, setValid] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<string>()
   const [hasFocus, setHasFocus] = useState<boolean>(false)
+  const [lostFocus, setLostFocus] = useState<boolean>(false)
 
   useEffect(() => {
     let tmpValid = true
+    let tmpErrorMessage: string | undefined
     if (type === 'number') {
-      if (
-        typeof internalValue !== 'number' ||
-        !isValidNumber(internalValue, min, max)
-      ) {
+      if (typeof internalValue !== 'number' || Number.isNaN(internalValue)) {
         tmpValid = false
+        tmpErrorMessage = 'Is not a valid number'
+      } else if (min !== undefined && internalValue < min) {
+        tmpValid = false
+        tmpErrorMessage = 'Cannot be less than ' + min
+      } else if (max !== undefined && internalValue > max) {
+        tmpValid = false
+        tmpErrorMessage = 'Cannot be greater than ' + max
       }
     } else if (type === 'text') {
       if (typeof internalValue !== 'string') {
         tmpValid = false
       } else if (required && !internalValue) {
         tmpValid = false
-      } else if (internalValue && regex && !regex.test(internalValue)) {
+        tmpErrorMessage =
+          typeof required === 'string' ? required : 'This field is required'
+      } else if (
+        internalValue &&
+        minLength &&
+        internalValue.length < minLength
+      ) {
         tmpValid = false
+        tmpErrorMessage = 'Minimum length must be greater than ' + minLength
+      } else if (
+        internalValue &&
+        maxLength &&
+        internalValue.length > maxLength
+      ) {
+        tmpValid = false
+        tmpErrorMessage = 'Maximum length must be less than ' + maxLength
+      } else if (
+        internalValue &&
+        regex &&
+        !(regex instanceof RegExp ? regex : regex.value).test(internalValue)
+      ) {
+        tmpValid = false
+        tmpErrorMessage =
+          regex instanceof RegExp
+            ? "Can't contain illegal character"
+            : regex.message
       }
     }
     onValid?.(tmpValid)
     setValid(tmpValid)
-  }, [internalValue, type, min, max, regex, required, onValid])
+    setErrorMessage(tmpErrorMessage)
+  }, [
+    internalValue,
+    type,
+    min,
+    max,
+    minLength,
+    maxLength,
+    regex,
+    required,
+    onValid,
+  ])
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     let newValue: string | number = event.target.value
@@ -87,45 +136,58 @@ const TextField: React.FC<TextFieldProps> = ({
   }
 
   return (
-    <div
-      className={classNames(
-        styles.textFieldInputContainer,
-        size === 'small' ? styles.small : undefined,
-        disabled ? styles.disabled : undefined,
-        !valid && hasFocus ? styles.error : undefined,
-      )}>
-      <input
-        id={id}
-        name={name ?? id}
-        type={type}
-        value={internalValue}
-        min={min}
-        max={max}
-        placeholder={placeholder}
-        disabled={disabled}
-        readOnly={readOnly}
-        className={styles.textfield}
-        onChange={handleChange}
-        onFocus={() => setHasFocus(true)}
-        onBlur={() => setHasFocus(false)}
-        data-testid={`test-${id}-textfield`}
-      />
-      {checked !== undefined && (
-        <label htmlFor={`${id}-checkbox`} className={styles.checkboxLabel}>
-          <input
-            id={`${id}-checkbox`}
-            type="checkbox"
-            checked={checked}
-            disabled={disabled}
-            className={styles.checkbox}
-            onChange={(event) => onCheck?.(event.target.checked)}
-          />
-          {checked && (
-            <span className={styles.checkboxIcon}>
-              <FontAwesomeIcon icon={faCheck} />
-            </span>
-          )}
-        </label>
+    <div className={styles.inputContainer}>
+      <div
+        className={classNames(
+          styles.textFieldInputContainer,
+          size === 'small' ? styles.small : undefined,
+          disabled ? styles.disabled : undefined,
+          !valid && (lostFocus || hasFocus) ? styles.error : undefined,
+        )}>
+        <input
+          id={id}
+          name={name ?? id}
+          type={type}
+          value={internalValue}
+          min={min}
+          max={max}
+          minLength={minLength}
+          maxLength={maxLength}
+          placeholder={placeholder}
+          disabled={disabled}
+          readOnly={readOnly}
+          className={styles.textfield}
+          onChange={handleChange}
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => {
+            setHasFocus(false)
+            setLostFocus(true)
+          }}
+          data-testid={`test-${id}-textfield`}
+        />
+        {checked !== undefined && (
+          <label htmlFor={`${id}-checkbox`} className={styles.checkboxLabel}>
+            <input
+              id={`${id}-checkbox`}
+              type="checkbox"
+              checked={checked}
+              disabled={disabled}
+              className={styles.checkbox}
+              onChange={(event) => onCheck?.(event.target.checked)}
+            />
+            {checked && (
+              <span className={styles.checkboxIcon}>
+                <FontAwesomeIcon icon={faCheck} />
+              </span>
+            )}
+          </label>
+        )}
+      </div>
+      {!valid && (lostFocus || hasFocus) && (
+        <div className={styles.errorContainer}>
+          <FontAwesomeIcon icon={faTriangleExclamation} />{' '}
+          {errorMessage ?? 'Unknown error'}
+        </div>
       )}
     </div>
   )
