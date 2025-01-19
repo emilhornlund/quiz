1b9c7bc474621f9f97b910a303af494f15abd907
Emil Hörnlund <emil.hornlund@me.com>
2025-01-19 14:26:43 +0100

    refactor(quiz): improve distribution rendering and styling
    - Introduced `getResultChips` helper function to simplify and streamline the rendering of distribution results for different question types.
    - Updated `ResultChip` to include an icon (FontAwesome's `faUserGroup`) for better visualization of participant counts.
    - Refactored SCSS styles for `QuestionResults` to enhance responsiveness and maintain consistent spacing across different screen sizes using `$mobile-layout-gutter`, `$tablet-layout-gutter`, and `$desktop-layout-gutter`.
    - Adjusted chip styles for better clarity, including hover effects and enhanced visual indicators for correct/incorrect results.
    - Improved code maintainability by consolidating duplicate logic for chip rendering based on `QuestionType`.
    
    These changes enhance the user experience by improving the visual appeal and clarity of the question results component.

--- packages/quiz/src/components/QuestionResults/QuestionResults.tsx
+++ packages/quiz/src/components/QuestionResults/QuestionResults.tsx
@@ -1,3 +1,5 @@
+import { faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { GameEventQuestionResults, QuestionType } from '@quiz/common'
 import React, { FC } from 'react'
 
@@ -18,7 +20,9 @@ const ResultChip: FC<ResultChipProps> = ({ value, count, correct }) => (
       correct ? styles.correct : styles.incorrect,
     )}>
     {value}
-    <span>{count}</span>
+    <span>
+      <FontAwesomeIcon icon={faUserGroup} /> {count}
+    </span>
   </div>
 )
 
@@ -26,49 +30,55 @@ export interface QuestionResultsProps {
   results: GameEventQuestionResults
 }
 
+const getResultChips = (results: GameEventQuestionResults, correct: boolean) =>
+  results.distribution
+    .filter((item) => item.correct === correct)
+    .sort((lhs, rhs) => rhs.count - lhs.count)
+    .map(({ value, count, correct }, index) => {
+      switch (results.type) {
+        case QuestionType.MultiChoice:
+          return (
+            <ResultChip
+              key={`${index}_${value}`}
+              value={value as string}
+              count={count}
+              correct={correct}
+            />
+          )
+        case QuestionType.TrueFalse:
+          return (
+            <ResultChip
+              key={`${index}_${value}`}
+              value={(value as boolean) ? 'True' : 'False'}
+              count={count}
+              correct={correct}
+            />
+          )
+        case QuestionType.Range:
+          return (
+            <ResultChip
+              key={`${index}_${value}`}
+              value={`${value as number}`}
+              count={count}
+              correct={correct}
+            />
+          )
+        case QuestionType.TypeAnswer:
+          return (
+            <ResultChip
+              key={`${index}_${value}`}
+              value={value as string}
+              count={count}
+              correct={correct}
+            />
+          )
+      }
+    })
+
 const QuestionResults: FC<QuestionResultsProps> = ({ results }) => (
   <div className={styles.questionResults}>
-    <div className={styles.chips}>
-      {results.type === QuestionType.MultiChoice &&
-        results.distribution.map(({ value, count, correct }, index) => (
-          <ResultChip
-            key={`${index}_${value}`}
-            value={value}
-            count={count}
-            correct={correct}
-          />
-        ))}
-
-      {results.type === QuestionType.TrueFalse &&
-        results.distribution.map(({ value, count, correct }, index) => (
-          <ResultChip
-            key={`${index}_${value}`}
-            value={value ? 'True' : 'False'}
-            count={count}
-            correct={correct}
-          />
-        ))}
-
-      {results.type === QuestionType.Range &&
-        results.distribution.map(({ value, count, correct }, index) => (
-          <ResultChip
-            key={`${index}_${value}`}
-            value={`${value}`}
-            count={count}
-            correct={correct}
-          />
-        ))}
-
-      {results.type === QuestionType.TypeAnswer &&
-        results.distribution.map(({ value, count, correct }, index) => (
-          <ResultChip
-            key={`${index}_${value}`}
-            value={value}
-            count={count}
-            correct={correct}
-          />
-        ))}
-    </div>
+    <div className={styles.chips}>{getResultChips(results, true)}</div>
+    <div className={styles.chips}>{getResultChips(results, false)}</div>
   </div>
 )
 
