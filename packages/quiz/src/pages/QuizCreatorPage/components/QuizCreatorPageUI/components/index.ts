63ec62d682275ac000229c747fa85f22e1230e7d
Emil Hörnlund <emil.hornlund@me.com>
2025-03-07 16:09:24 +0100

    Oooops quiz settings modal

--- packages/quiz/src/pages/QuizCreatorPage/components/QuizCreatorPageUI/components/index.ts
+++ packages/quiz/src/pages/QuizCreatorPage/components/QuizCreatorPageUI/components/index.ts
@@ -1,7 +1,9 @@
 export type { GameModeSelectionModalProps } from './GameModeSelectionModal'
 export type { QuestionEditorProps } from './QuestionEditor'
 export type { QuestionPickerItem, QuestionPickerProps } from './QuestionPicker'
+export type { QuizSettingsModalProps } from './QuizSettingsModal'
 
 export { default as GameModeSelectionModal } from './GameModeSelectionModal'
 export { default as QuestionEditor } from './QuestionEditor'
 export { default as QuestionPicker } from './QuestionPicker'
+export { default as QuizSettingsModal } from './QuizSettingsModal'
